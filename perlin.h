72ace5d486c7fd4a9ccdf3af553c7afbfd8058b5
toastoffee <931391442@qq.com>
2024-06-08 19:49:31 +0800

    add random vectors

--- perlin.h
+++ perlin.h
@@ -18,9 +18,9 @@
 class perlin {
 public:
     perlin() {
-        randfloat = new double[point_count];
+        randvec = new vec3[point_count];
         for (int i = 0; i < point_count; i++) {
-            randfloat[i] = random_double();
+            randvec[i] = unit_vector(vec3::random(-1,1));
         }
 
         perm_x = perlin_generate_perm();
@@ -29,7 +29,7 @@ public:
     }
 
     ~perlin() {
-        delete[] randfloat;
+        delete[] randvec;
         delete[] perm_x;
         delete[] perm_y;
         delete[] perm_z;
@@ -39,30 +39,28 @@ public:
         auto u = p.x() - floor(p.x());
         auto v = p.y() - floor(p.y());
         auto w = p.z() - floor(p.z());
-        u = u*u*(3-2*u);
-        v = v*v*(3-2*v);
-        w = w*w*(3-2*w);
+
 
         auto i = int(floor(p.x()));
         auto j = int(floor(p.y()));
         auto k = int(floor(p.z()));
-        double c[2][2][2];
+        vec3 c[2][2][2];
 
         for (int di=0; di < 2; di++)
             for (int dj=0; dj < 2; dj++)
                 for (int dk=0; dk < 2; dk++)
-                    c[di][dj][dk] = randfloat[
+                    c[di][dj][dk] = randvec[
                             perm_x[(i+di) & 255] ^
                             perm_y[(j+dj) & 255] ^
                             perm_z[(k+dk) & 255]
                     ];
 
-        return trilinear_interp(c, u, v, w);
+        return perlin_interp(c, u, v, w);
     }
 
 private:
     static const int point_count = 256;
-    double* randfloat;
+    vec3* randvec;
     int* perm_x;
     int* perm_y;
     int* perm_z;
@@ -87,15 +85,22 @@ private:
         }
     }
 
-    static double trilinear_interp(double c[2][2][2], double u, double v, double w) {
+
+    static double perlin_interp(const vec3 c[2][2][2], double u, double v, double w) {
+        auto uu = u*u*(3-2*u);
+        auto vv = v*v*(3-2*v);
+        auto ww = w*w*(3-2*w);
         auto accum = 0.0;
+
         for (int i=0; i < 2; i++)
             for (int j=0; j < 2; j++)
-                for (int k=0; k < 2; k++)
-                    accum += (i*u + (1-i)*(1-u))
-                             * (j*v + (1-j)*(1-v))
-                             * (k*w + (1-k)*(1-w))
-                             * c[i][j][k];
+                for (int k=0; k < 2; k++) {
+                    vec3 weight_v(u-i, v-j, w-k);
+                    accum += (i*uu + (1-i)*(1-uu))
+                             * (j*vv + (1-j)*(1-vv))
+                             * (k*ww + (1-k)*(1-ww))
+                             * dot(c[i][j][k], weight_v);
+                }
 
         return accum;
     }
--- texture.h
+++ texture.h
@@ -99,7 +99,7 @@ public:
 
 
     color value(double u, double v, const point3& p) const override {
-        return color(1,1,1) * noise.noise(scale * p);
+        return color(1,1,1) * 0.5 * (1.0 + noise.noise(scale * p));
     }
 
 private:
