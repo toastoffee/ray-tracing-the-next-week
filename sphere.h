4efec66c71723d9e007c5cc3e64b539bcd946fea
toastoffee <931391442@qq.com>
2024-06-08 18:37:05 +0800

    add two spheres scene

--- sphere.h
+++ sphere.h
@@ -63,6 +63,7 @@ class sphere : public hittable {
         rec.p = r.at(rec.t);
         vec3 outward_normal = (rec.p - center) / radius;
         rec.set_face_normal(r, outward_normal);
+        get_sphere_uv(outward_normal, rec.u, rec.v);
         rec.mat = mat;
 
         return true;
@@ -83,6 +84,21 @@ class sphere : public hittable {
         // Linearly interpolate from center1 to center2 according to time, where t=0 yields
         // center1, and t=1 yields center2.
         return center1 + time*center_vec;
+    }
+
+    static void get_sphere_uv(const point3& p, double& u, double& v) {
+        // p: a given point on the sphere of radius one, centered at the origin.
+        // u: returned value [0,1] of angle around the Y axis from X=-1.
+        // v: returned value [0,1] of angle from Y=-1 to Y=+1.
+        //     <1 0 0> yields <0.50 0.50>       <-1  0  0> yields <0.00 0.50>
+        //     <0 1 0> yields <0.50 1.00>       < 0 -1  0> yields <0.50 0.00>
+        //     <0 0 1> yields <0.25 0.50>       < 0  0 -1> yields <0.75 0.50>
+
+        auto theta = acos(-p.y());
+        auto phi = atan2(-p.z(), p.x()) + pi;
+
+        u = phi / (2*pi);
+        v = theta / pi;
     }
 
 };
