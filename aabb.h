0d3508d40162474a92152ce2225fa1ef80dd25c4
toastoffee <931391442@qq.com>
2024-06-09 13:03:57 +0800

    add instance translate

--- aabb.h
+++ aabb.h
@@ -106,4 +106,12 @@ private:
 const aabb aabb::empty    = aabb(interval::empty,    interval::empty,    interval::empty);
 const aabb aabb::universe = aabb(interval::universe, interval::universe, interval::universe);
 
+aabb operator+(const aabb& bbox, const vec3& offset) {
+    return aabb(bbox.x + offset.x(), bbox.y + offset.y(), bbox.z + offset.z());
+}
+
+aabb operator+(const vec3& offset, const aabb& bbox) {
+    return bbox + offset;
+}
+
 #endif //RAY_TRACING_THE_NEXT_WEEK_AABB_H
--- hittable.h
+++ hittable.h
@@ -46,5 +46,34 @@ class hittable {
     virtual aabb bounding_box() const = 0;
 };
 
+class translate : public hittable {
+public:
+    translate(shared_ptr<hittable> object, const vec3& offset)
+            : object(object), offset(offset)
+    {
+        bbox = object->bounding_box() + offset;
+    }
+
+    bool hit(const ray& r, interval ray_t, hit_record& rec) const override {
+        // Move the ray backwards by the offset
+        ray offset_r(r.origin() - offset, r.direction(), r.time());
+
+        // Determine whether an intersection exists along the offset ray (and if so, where)
+        if (!object->hit(offset_r, ray_t, rec))
+            return false;
+
+        // Move the intersection point forwards by the offset
+        rec.p += offset;
+
+        return true;
+    }
+
+    aabb bounding_box() const override { return bbox; }
+
+private:
+    shared_ptr<hittable> object;
+    vec3 offset;
+    aabb bbox;
+};
 
 #endif
